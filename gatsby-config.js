904be4371d3176210dc8a9cf4bde41fdba40908d
Dhaval Patel <dhavalp0321@gmail.com>
2020-11-14 18:31:12 -0500

    added topSection component and removed some code--styling coming up next

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,8 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
+    `gatsby-transformer-sharp`, 
+    `gatsby-plugin-sharp`,
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: "gatsby-source-filesystem",
---
+++ src/components/TopSection.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+//Top Section for website, will have A heading text, some subheading text, a 'let's grab coffee link, an da picture
+function TopSection() {
+  return (
+    <div className="top-section">
+    <div className="intro-blurb">
+      <h2>Hi, I'm Dhaval</h2>
+      <h3>Software Engineer and Aspiring Surfer Boy</h3>
+      <button type='button'> Let's grab some coffee! (Virtually)</button>
+    </div>
+    <img src="../img/dhaval-top-pic.png"/>
+    </div>
+  )
+}
+export default TopSection
