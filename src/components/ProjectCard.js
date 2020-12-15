013d57fea66dc5a8de6e1e08537ecb57aa375dcc
Dhaval Patel <dhavalp0321@gmail.com>
2020-12-14 21:36:38 -0500

    some button styling

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,14 +1,15 @@
-import React from "React";
-import "./css/projectcard.scss"
+import React from 'React';
+import './css/projectcard.scss';
 // take in image as prop and project name
-function ProjectCard(props){
+function ProjectCard(props) {
   return (
-    <div className='project-card'> 
-    <div className='canvas'>
-    <h2>{props.projectName}</h2>
+    <div className="project-card">
+      <div className="canvas">
+        <h2>{props.projectName}</h2>
+        <button type="button"> Read more </button>
+      </div>
     </div>
-  </div>
-  )
-};
+  );
+}
 
 export default ProjectCard;
